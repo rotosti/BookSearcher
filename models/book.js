70df2dd104984f175644b893c8d41fb2f13901b2
Tom Siemion <tsiemion07@gmail.com>
2021-06-05 15:38:58 -0500

    initial model set up

---
+++ models/book.js
@@ -0,0 +1,24 @@
+const Mongoose = require('mongoose');
+const Schema = Mongoose.Schema;
+
+const bookSchema = new Schema({
+    title: {
+        type: String
+    },
+    authors: {
+        type: String
+    },
+    description: {
+        type: String
+    },
+    image: {
+        type: String
+    },
+    link: {
+        type: String
+    }
+});
+
+const Book = Mongoose.model('Book', bookSchema);
+
+module.exports = Book;
