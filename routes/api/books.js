d713e0838c5e8cf493e876f7dc0755348295500b
Tom Siemion <tsiemion07@gmail.com>
2021-06-05 21:10:16 -0500

    initial route setup

---
+++ routes/api/books.js
@@ -0,0 +1,9 @@
+const router = require('express').Router();
+const controller = require('../../controller/bookController'); // add controller
+
+router.route('/')
+    .get(controller.getAllSaved)
+    .post(controller.saveBook);
+
+router.route('/:id')
+    .delete(controller.removeSaved);
