009543454f20b9c56835e78dfe5cc5cd2b8556bb
Tom Siemion <tsiemion07@gmail.com>
2021-06-05 22:15:31 -0500

    added hero component

---
+++ client/src/components/Hero/index.js
@@ -0,0 +1,14 @@
+import React from 'react';
+
+function Hero() {
+    return (
+        <div className='container'>
+            <div className='d-flex flex-column justify-content-center py-5 text-center border border-dark border-1 m-4'>
+                <h1>Google Books Search</h1>
+                <h2>Search for and Save Books of Interest</h2>
+            </div>
+        </div>
+    );
+}
+
+export default Hero;
